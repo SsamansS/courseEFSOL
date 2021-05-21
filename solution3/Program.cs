940243d9c1217caaa6028d1038a72d2a7be810e9
SsamansS <nurlanov.aman@bk.ru>
2021-05-21 18:43:21 +0600

    lesson9_solution3 недоделана. Можете посмотреть у Айданы

---
+++ solution3/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace lesson9_solution3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 3, 32, 0, 32, 3, 2, 1 };
+            foreach (int i in SubArray(array, 2, 9))
+            {
+                Console.WriteLine(i);
+            }
+        }
+        static int[] MyReverse(int[] array)
+        {
+            int[] arr = new int[array.Length];
+            for (int item = 0; item < array.Length; item++)
+            {
+                arr[item] = array[array.Length - 1 - item];
+            }
+
+            return arr;
+        }
+
+        /*static int[] SubArray(int[] array, int index, int count)
+        {
+            int[] arr = new int[count];
+            for(int i = 0; i<count;i++)
+            {
+                if (i >= index && i < count)
+                {
+                    
+
+                }
+                else
+                    arr[i] = 1;
+            }
+            return array;
+        }*/
+
+    }
+}
