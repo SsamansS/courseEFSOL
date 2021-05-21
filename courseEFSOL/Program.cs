53fcff87af02fd2adfce521b37fa6b6df3ab5bc6
SsamansS <nurlanov.aman@bk.ru>
2021-05-21 13:00:33 +0600

    Добавьте файлы проекта.

---
+++ courseEFSOL/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace courseEFSOL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ solution3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace solution3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            int z = 15;
+            
+            Console.WriteLine(x += y >> x++ * z);
+            Console.WriteLine(z = ++x & y * 5);
+            Console.WriteLine(y /= x + 5 | z);
+            Console.WriteLine(z = x++ & y * 5);
+            Console.WriteLine(x = y << x++ ^ z);
+        }
+    }
+}
