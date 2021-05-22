f5452329ecba2af1ae1565c4b7f10553aa033345
SsamansS <nurlanov.aman@bk.ru>
2021-05-22 20:12:33 +0600

    Start basic course. Done lesson1_solution2

---
+++ lesson1_solution2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lesson1_solution2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ширину прямоугольника");
+            double side1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите высоту прямоугольника");
+            double side2 = double.Parse(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(side1, side2);
+            Console.WriteLine($"Площадь прямоугольника = {rectangle.Area}");
+            Console.WriteLine($"Периметр прямоугольника = {rectangle.Perimeter}");
+        }
+    }
+}
