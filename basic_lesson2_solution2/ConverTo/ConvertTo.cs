72b14a2a2a5d6c13d46845ca9b2d7c3928474622
SsamansS <nurlanov.aman@bk.ru>
2021-05-23 21:35:14 +0600

    done basic_lesson2_solution2

---
+++ basic_lesson2_solution2/ConverTo/ConvertTo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_lesson2_solution2.ConvertToo
+{
+    class ConvertTo
+    {
+        public double Dollar;
+        public double Euro;
+        public double Ruble;
+    }
+}
---
+++ basic_lesson2_solution2/Converter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using basic_lesson2_solution2.ConvertToo;
+
+namespace basic_lesson2_solution2
+{
+    class Converter
+    {
+
+
+        public Converter(double usd, double eur, double rub) //переводит гривны
+        {
+            ConvertTo hryvniasTo = new ConvertTo();
+            hryvniasTo.Dollar = 0.036 * usd;
+            hryvniasTo.Euro = 0.030 * eur;
+            hryvniasTo.Ruble = 2.68 * rub;
+            Console.WriteLine("Перевод гривн в");
+            Show(hryvniasTo);
+            
+
+            ConvertTo toHryvnias = new ConvertTo();
+            toHryvnias.Dollar = 27.43 * usd;
+            toHryvnias.Euro = 33.41 * eur;
+            toHryvnias.Ruble = 0.37 * rub;
+            Console.WriteLine("Перевод на гривны из");
+            Show(toHryvnias);
+        }
+
+        public void Show(ConvertTo hryvnias)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine($"Доллары - {hryvnias.Dollar}");
+            Console.WriteLine($"Евро - {hryvnias.Euro}");
+            Console.WriteLine($"Рубли - {hryvnias.Ruble}");
+            Console.ForegroundColor = ConsoleColor.Gray;
+        }
+    }
+}
