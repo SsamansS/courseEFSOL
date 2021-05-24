4179985e874b87dd0590974dbb3cd9e0569b2ca1
SsamansS <nurlanov.aman@bk.ru>
2021-05-24 17:35:32 +0600

    add условие задания basuc 1.1

--- lesson1_solution2/Program.cs
+++ lesson1_solution2/Program.cs
@@ -2,6 +2,19 @@
 
 namespace basic_lesson1_solution2
 {
+    /*
+     * Задание 2
+    Используя Visual Studio, создайте проект по шаблону Console Application.
+    Требуется: Создать класс с именем Rectangle.
+        В теле класса создать два поля, описывающие длины сторон double side1, side2.
+    Создать пользовательский конструктор Rectangle(double side1, double side2), в теле которого
+    поля side1 и side2 инициализируются значениями аргументов.
+    Создать два метода, вычисляющие площадь прямоугольника - double AreaCalculator() и периметр
+    прямоугольника - double PerimeterCalculator().
+    Создать два свойства double Area и double Perimeter с одним методом доступа get.
+    Написать программу, которая принимает от пользователя длины двух сторон прямоугольника и выводит
+    на экран периметр и площадь.
+    */
     class Program
     {
         static void Main(string[] args)
