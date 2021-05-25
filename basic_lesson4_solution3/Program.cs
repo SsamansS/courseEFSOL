b43e8d63bd7105605d1936e2e3a2d982f2fcb2fd
SsamansS <nurlanov.aman@bk.ru>
2021-05-25 16:03:22 +0600

    done basic_lesson4_solution3

---
+++ basic_lesson4_solution3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace basic_lesson4_solution3
+{
+    /*
+     * Задание 3
+    Используя Visual Studio, создайте проект по шаблону Console Application.
+    Требуется:
+    Создайте 2 интерфейса IPlayable и IRecodable. В каждом из интерфейсов создайте по 3 метода void
+    Play() / void Pause() / void Stop() и void Record() / void Pause() / void Stop() соответственно.
+    Создайте производный класс Player от базовых интерфейсов IPlayable и IRecodable.
+    Написать программу, которая выполняет проигрывание и запись. 
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Recordable");
+            IRecodable recodable = new Player();
+            recodable.Pause();
+            recodable.Stop();
+            recodable.Record();
+            Console.WriteLine("\nPlayable");
+            IPlayable playable = new Player();
+            playable.Pause();
+            playable.Stop();
+            playable.Play();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ basic_lesson5_solution2/ThisArray.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_lesson5_solution2
+{
+    class ThisArray
+    {
+        //public static int n;
+        int[] array;
+
+        public int this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+        public void Sortin()
+        {
+            //int[] arr = Array.Sort(array, 0, array.Length - 1);
+            //foreach(int i in Array.Sort(array, 0, array.Length - 1))
+        }
+
+    }
+}
