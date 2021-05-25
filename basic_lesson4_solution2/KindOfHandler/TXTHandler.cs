090f9d920f65f2f0ce4073861add35c571b51568
SsamansS <nurlanov.aman@bk.ru>
2021-05-25 11:45:18 +0600

    done basic_lesson4_solution2

---
+++ basic_lesson4_solution2/KindOfHandler/TXTHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_lesson4_solution2.KindOfHandler
+{
+    class TXTHandler:AbstractHandler
+    {
+        public override void Open()
+        {
+            Console.WriteLine("Открыт TXT");
+        }
+        public override void Create()
+        {
+            Console.WriteLine("Создан TXT");
+        }
+        public override void Chenge()
+        {
+            Console.WriteLine("Изменение TXT");
+        }
+        public override void Save()
+        {
+            Console.WriteLine("Сохранение TXT");
+        }
+        public override void Show()
+        {
+            Console.WriteLine("\nTXT:");
+            this.Open();
+            this.Create();
+            this.Chenge();
+            this.Save();
+        }
+    }
+}
