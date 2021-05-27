035e505dc3f97e7c92c80659c5e00b4fee352c6f
SsamansS <nurlanov.aman@bk.ru>
2021-05-28 02:39:54 +0600

    done basic_lesson9_solution3

--- basic_lesson9_solution3/Program.cs
+++ basic_lesson9_solution3/Program.cs
@@ -1,7 +1,5 @@
 ﻿using System;
 
-namespace basic_lesson9_solution3
-{
     /*
      * Задание 3
     Используя Visual Studio, создайте проект по шаблону Console Application.
@@ -9,16 +7,31 @@ namespace basic_lesson9_solution3
     среднее арифметическое возвращаемых значений методов, сообщенных с делегатами в массиве.
     Методы, сообщенные с делегатами из массива, возвращают случайное значение типа int.
     */
+
+namespace basic_lesson9_solution3
+{
+    public delegate double MyDelegate();
+    public delegate double MyDelegatess(MyDelegate[] myDelegates);
+
     class Program
     {
         static void Main(string[] args)
         {
-            int[] vs = { 1, 2, 32, 322};
-            foreach(int i in vs[1..3])
+            MyDelegate myDelegate = () => 
             {
-                Console.WriteLine(i);
-            }
-            Console.WriteLine(vs[1..3]);
+                Random random = new Random();
+                return random.Next(1,7);
+            };
+
+            MyDelegate[] myDelegates = { myDelegate, myDelegate };
+
+            MyDelegatess myDelegates1 = (myDelegates) =>
+            {
+                return (myDelegates[0].Invoke() + myDelegates[1].Invoke()) / myDelegates.Length ;
+            };
+
+
+            Console.WriteLine(myDelegates1.Invoke(myDelegates));
         }
     }
 }
