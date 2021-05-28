fcffd37106e1f4beb977e19599bf211c5f23cf5a
SsamansS <nurlanov.aman@bk.ru>
2021-05-28 17:07:08 +0600

    done basic_lesson10_solution4(в basic_lesson10_solution2and4)

--- basic_lesson10_solution2/MyList.cs
+++ basic_lesson10_solution2/MyList.cs
@@ -1,14 +1,16 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace basic_lesson10_solution2
+namespace basic_lesson10_solution2and4
 {
-    class MyList<T>
+  
+    public class MyList<T> 
     {
-        T[] _List;
+        public T[] List;
         int _Count;
 
         public int Count
@@ -20,36 +22,39 @@ namespace basic_lesson10_solution2
         {
             get
             {
-                return _List[index];
+                return List[index];
             }
             set
             {
-                _List[index] = value;
+                List[index] = value;
             }
         }
 
-        public MyList(int count)
+        
+           
+    public MyList(int count)
         {
-            this._List = new T[count];
+            this.List = new T[count];
             this._Count = count;
         }
         public MyList()
         {
-            this._List = new T[1];
+            this.List = new T[1];
             this._Count = 1;
         }
         public void Add(T NewItem)
         {
-            T[] NewList = new T[_List.Length + 1];
+            T[] NewList = new T[List.Length + 1];
             NewList[^1] = NewItem;
-            this._List = NewList;
-            this._Count = _List.Length;
+            this.List = NewList;
+            this._Count = List.Length;
+            
         }
         public T GetItem(int index)
         {
-            T Item = _List[index];
+            T Item = List[index];
             return Item;
         }
-        
+
     }
 }
