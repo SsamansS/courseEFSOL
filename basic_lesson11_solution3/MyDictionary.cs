14f12c170455c5f199dd4b9742fc8b6dd3245d66
SsamansS <nurlanov.aman@bk.ru>
2021-05-31 17:32:06 +0600

    done basic_lesson11_solution3

---
+++ basic_lesson11_solution3/MyDictionary.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_lesson11_solution3
+{
+    class MyDictionary<TKey, TValue>
+    {
+        List<TKey> keys;
+
+        List<TValue> values;
+
+        public int Count;
+        public TValue this[TKey key]
+        {
+            get
+            {
+                return values[LookForIndex(key)];
+            }
+        }
+
+        public MyDictionary(int lenght)
+        {
+            this.Count = lenght;
+            this.keys = new List<TKey>(lenght);
+            this.values = new List<TValue>(lenght);
+        }
+        public MyDictionary()
+        {
+            this.Count = 1;
+            this.keys = new List<TKey>(1);
+            this.values = new List<TValue>(1);
+        }
+
+
+        public void Add(TKey key, TValue value)
+        {
+            this.keys.Add(key);
+            this.values.Add(value);
+            this.Count = values.Count;
+        }
+
+        int LookForIndex(TKey key)
+        {
+            int index = -1;
+            for (int i = 0; i < Count; i++)
+            {
+                if ($"{this.keys[i]}" == $"{key}")
+                    index = i;
+            }
+            
+            return index;
+        }
+
+    }
+}
---
+++ basic_lesson11_solution3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+/*
+ * Задание 3
+Используя Visual Studio, создайте проект по шаблону Console Application.
+Создайте класс MyDictionary<TKey,TValue>. Реализуйте в простейшем приближении возможность
+использования его экземпляра аналогично экземпляру класса Dictionary (Урок 6 пример 5).
+Минимально требуемый интерфейс взаимодействия с экземпляром, должен включать метод
+добавления пар элементов, индексатор для получения значения элемента по указанному индексу и
+свойство только для чтения для получения общего количества пар элементов. 
+*/
+
+namespace basic_lesson11_solution3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> dsfs = new Dictionary<string, string>(4);
+            dsfs.Add("key", "value");
+            Console.WriteLine(dsfs["keydf"]);
+            Console.WriteLine(dsfs.Count);
+        }
+    }
+}
