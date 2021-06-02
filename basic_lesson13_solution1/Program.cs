cc63760591d7ce035675e4da01cc0bae5fab500d
SsamansS <nurlanov.aman@bk.ru>
2021-06-02 16:59:22 +0600

    done basic_lesson16_solution4

--- basic_lesson13_solution1/Program.cs
+++ basic_lesson13_solution1/Program.cs
@@ -21,126 +21,57 @@ namespace basic_lesson13_solution1
         static object locker = new object();
         static void Main(string[] args)
         {
-            Thread myThread = new Thread(new ThreadStart(PrintColumn));
-            myThread.Start(); // запускаем поток
 
-            Monitor.Enter(locker);
-
-
-            while (true)
-            {
-                PrintColumn();
-                Thread.Sleep(300);
-                Console.Clear();
-            }
 
             //foreach (var item in Enumerable.Range(1, 100))
             //{
-            //    new Thread(PrintColumn).Start();
+                new Thread(PrintColumn).Start();
             //}
 
         }
 
         static void PrintColumn()
         {
-            Random random = new Random();
-            int len = random.Next(0, Console.WindowHeight);
-            int indent = random.Next(0, Console.WindowWidth);
-            for (int i = 0;  i < len; i++)
+            while (true)
             {
-                Console.ForegroundColor = ConsoleColor.DarkGreen;
-                if (i == len - 1) { Console.ForegroundColor = ConsoleColor.White; }
-                if (i == len - 2) { Console.ForegroundColor = ConsoleColor.Green; }
+                Random random = new Random();
+                int len = random.Next(3, Console.WindowHeight);
+                int indent = random.Next(0, Console.WindowWidth);
+                    lock (locker)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkGreen;
+                        for (int i = 0; i < len; i++)
+                        {
+                            if (i == len - 1) { Console.ForegroundColor = ConsoleColor.White; }
+                            if (i == len - 2) { Console.ForegroundColor = ConsoleColor.Green; }
 
-                Console.SetCursorPosition(len, indent + i);
-                Console.Write(GetRandomChar());
+                            Console.SetCursorPosition(len, indent + i);
+                            string str = $"{new string(' ', indent)}{GetRandomChar()}";
 
-                Console.WriteLine($"{new string(' ', indent)}{GetRandomChar()}");
-                Console.ResetColor();
+                            Console.WriteLine(str);
+                            str = $"{new string(' ', indent)} ";
+                        Console.ResetColor();
+                        }
+                        if (indent++ >= Console.WindowHeight - len - 1)
+                        {
+                            for (var i = 0; i < len; i++)
+                            {
+                                Console.SetCursorPosition(len, indent + i);
+                                Console.Write(' ');
+                            }
+                            break;
+                        }
+                        Thread.Sleep(300);
+                    }
+                
             }
         }
         static char GetRandomChar()
         {
             Random random = new Random();
             string s = random.ToString().ToUpper();
-            char c = s[random.Next(0, s.Length - 1)];
+            char c = s[random.Next(0, 9)];//s.Length - 1)];
             return c;
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        /*
-        static readonly object locker = new object();  //Объект синхронизации доступа к разделяемому ресурсу (объект блокировки)
-
-        static void Main(string[] args)
-        {
-            Console.SetWindowSize(100, 30);
-            Console.OutputEncoding = Encoding.UTF8;
-            Console.CursorVisible = false;
-
-            foreach (var item in Enumerable.Range(1, 100))
-            {
-                new Thread(NewMatrix).Start();
-            }
-        }
-        static void NewMatrix()
-        {
-            string yMatrix = " ";
-            int xPosition, yPosition, sleep, length;
-
-            while (true)
-            {
-                xPosition = new Random().Next(0, Console.WindowWidth);
-                yPosition = 0;
-                sleep = new Random().Next(50, 500);
-                length = new Random().Next(3, 10);
-
-                while (true)
-                {
-                    for (var i = 0; i < length; i++)
-                        yMatrix += ((char)new Random().Next(33, 126)).ToString();
-
-                    lock (locker)
-                    {
-                        Console.ForegroundColor = ConsoleColor.DarkGreen;
-                        for (var i = 0; i < length; i++)
-                        {
-                            if (i == length - 2) Console.ForegroundColor = ConsoleColor.Green;
-                            if (i == length - 1) Console.ForegroundColor = ConsoleColor.White;
-                            Console.SetCursorPosition(xPosition, yPosition + i);
-                            Console.Write(yMatrix[i]);
-                        }
-
-                        if (yPosition++ >= Console.WindowHeight - length - 1)
-                        {
-                            for (var i = 0; i < length; i++)
-                            {
-                                Console.SetCursorPosition(xPosition, yPosition + i);
-                                Console.Write(' ');
-                            }
-                            break;
-                        }
-                    }
-                    yMatrix = " ";
-                    Thread.Sleep(sleep);
-                }
-            }
-        }*/
     }
 }
---
+++ basic_lesson16_solution4/Date.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_lesson16_solution4
+{
+    enum Months
+    {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        Septemer = 9,
+        October = 10,
+        November = 11,
+        December = 12
+    }
+    class Date
+    {
+        byte _Day;
+        byte _Month;
+
+        public byte Day
+        {
+            get { return _Day; }
+            set
+            {
+                if (value != 0)
+                {
+                    if (this._Month <= 7)
+                    {
+                        if (this.Year % 4 == 0 && this._Month == 2)
+                        {
+                            if (value <= 29)
+                                _Day = value;
+                        }
+                        else if (this._Month == 2)
+                        {
+                            if (value <= 28)
+                                _Day = value;
+                        }
+                        else if (this._Month % 2 != 0)
+                        {
+                            if (value <= 31)
+                                _Day = value;
+                        }
+                        else
+                        {
+                            if (value <= 30)
+                                _Day = value;
+                        }
+                    }
+                    else
+                    {
+                        if (this._Month % 2 != 0)
+                        {
+                            if (value <= 30)
+                                _Day = value;
+                        }
+                        else// if (this.Month % 2 == 0)
+                        {
+                            if (value <= 31)
+                                _Day = value;
+                        }
+
+                    }
+                }
+            }
+        }
+        public byte Month
+        {
+            get { return _Month; }
+            set
+            {
+                if (value <= 12 && value != 0)
+                    _Month = value;
+            }
+        }
+        public int Year { get; set; }
+
+
+
+
+
+        public static int operator -(Date date1, Date date2)
+        {
+            int Days1 = date1.DaysInDate(date1);
+            int Days2 = date2.DaysInDate(date2);
+            return Days1 - Days2;
+        }
+
+        public static Date operator +(Date Date, int days)
+        {
+            if(days >= 0)
+            {
+                while(days >= 365)
+                {
+                    if (Date.Year % 4 == 0)
+                    {
+                        days -= 366;
+                    }
+                    else
+                        days -= 365;
+                    Date.Year++;
+                }
+                while(days >= 28)
+                {
+                    if (Date.Month <= 7)
+                    {
+                        if (Date.Year % 4 == 0 && Date.Month == 2)
+                        {
+                            days -= 29;
+                        }
+                        else if (Date.Month == 2)
+                        {
+                            days -= 28;
+                        }
+                        else if (Date.Month % 2 != 0)
+                        {
+                            days -= 31;
+                        }
+                        else
+                        {
+                            days -= 30;
+                        }
+                        Date.Month++;
+                    }
+                    else
+                    {
+                        if (Date.Month % 2 != 0)
+                        {
+                            days -= 30;
+                        }
+                        else
+                        {
+                            days -= 31;
+                        }
+
+                    }
+                }
+                Date.Day += (byte)days;
+            }
+            return Date;
+        }
+
+
+
+
+
+
+
+
+
+
+
+        int DaysInDate(Date date)
+        {
+            int Days = ((date.Year-1) / 4) * 366 + (date.Year -1 - (date.Year-1) / 4) * 365;
+            Days += DaysInMonth(date);
+            return Days;
+        }
+        int DaysInMonth(Date date)
+        {
+            int Days = date.Day;
+
+            for (int i = 1; i < date.Month; i++)
+            {
+                if (date.Month <= 7)
+                {
+                    if (date.Year % 4 == 0 && i == 2)
+                    {
+                        Days += 29;
+                    }
+                    else if (i == 2)
+                    {
+                        Days += 28;
+                    }
+                    else if (i % 2 != 0)
+                    {
+                        Days += 31;
+                    }
+                    else
+                    {
+                        Days += 30;
+                    }
+                }
+                else
+                {
+                    if (i % 2 != 0)
+                    {
+                        Days += 30;
+                    }
+                    else
+                    {
+                        Days += 31;
+                    }
+
+                }
+            }
+            return Days;
+        }
+    }
+}
