ca7da51994eaa9d9222fe5de1d2a7700cb058ef3
SsamansS <nurlanov.aman@bk.ru>
2021-06-05 23:19:20 +0600

    done prof_lesson3_2

---
+++ prof_lesson2_4/MyOrderedDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prof_lesson2_4
+{
+    class MyOrderedDictionary
+    {
+        OrderedDictionary dictionary = new OrderedDictionary();
+        public List<object> Keys;
+        public object this[object key]
+        {
+            get { return dictionary[key]; }
+            set {
+                Keys.Add(key);
+                dictionary[key] = value; }
+        }
+
+        public bool Equals(ICollection key2)
+        {
+            if (this.dictionary.Keys == key2)
+                return true;
+            return false;
+        }
+
+        public int GetHashCode(object obj)
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(object key, object value)
+        {
+            this.dictionary.Add(key, value);
+            this.Keys.Add(key);
+        }
+    }
+}
---
+++ prof_lesson3_2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+/*
+ * Задание 2 
+Создайте файл, запишите в него произвольные данные и закройте файл. Затем снова откройте
+этот файл, прочитайте из него данные и выведете их на консоль.
+*/
+
+namespace prof_lesson3_2
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string path = @"D:\Test.txt";
+            File.Create(path).Close();
+            File.WriteAllText(path, "dfddsfsdftgrhytrhytrhrtlg" +
+                "dfdfdfds");
+
+            Console.ReadKey();
+
+            string s = File.ReadAllText(path, Encoding.Default);
+            Console.WriteLine(s);
+
+
+
+        }
+    }
+}
