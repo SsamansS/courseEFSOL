f5ec26361221a6f10bad625750c4a8c0cf9713e6
SsamansS <nurlanov.aman@bk.ru>
2021-06-30 18:56:16 +0600

    done AsyncP_lesson2_3 and AsyncP_lesson2_4

---
+++ AsyncP_lesson2_3/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Threading;
+using System.Threading.Tasks;
+
+/*
+ * Задание 3
+Создайте проект по шаблону "Console Application". Создайте метод «private static int[]
+SortArray(bool isAscending, params int[] array)». Метод должен отсортировать массив и вернуть
+отсортированный массив в виде результата. Если параметр isAscending равен true - сортировать
+по возрастанию, если false - сортировать по убыванию. Как организовать алгоритм сортировки,
+полностью зависит от вашего выбора. Вызвать метод SortArray в контексте задачи для большого
+массива типа int. Результат задачи обработать в продолжении, где нужно вывести на экран
+консоли все элементы массива через запятую.
+*/
+
+namespace AsyncP_lesson2_3
+{
+    public class DescendingComparer : IComparer
+    {
+        readonly CaseInsensitiveComparer comparer = new CaseInsensitiveComparer();
+
+        public int Compare(object x, object y)
+        {
+            int result = comparer.Compare(y, x);
+            return result;
+        }
+    }
+    public struct Box
+    {
+        public bool isAscending;
+        public int[] array;
+        public Box(bool IsAscending, params int[] Array)
+        {
+            this.isAscending = IsAscending;
+            this.array = Array;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task<int[]> task1 = new Task<int[]>(SortArray, new Box(false, 1, 3, 32, -32, 32, 45, 6, 06, -434));
+            task1.Start();
+
+            Task task2 = new Task(new Action(() => WriteChar(task1.Result.Length)));
+            task2.Start();
+
+            foreach (int item in task1.Result)
+            {
+                Thread.Sleep(500);
+                Console.Write(item);
+            }
+        }
+
+        private static void WriteChar(int len)
+        {
+            Thread.Sleep(500);
+            for (int i = 0; i < len; i++)
+            {
+                Thread.Sleep(500);
+                Console.Write(", ");
+            }
+            Console.WriteLine("Метод " + nameof(WriteChar) + " выполнен");
+        }
+
+        private static int[] SortArray(object arg)
+        {
+            Box box = (Box)arg;
+            return SortArray(box.isAscending, box.array);
+        }
+        private static int[] SortArray(bool isAscending, params int[] array)
+        {
+            if (isAscending == true)
+            {
+                Array.Sort(array);
+                return array;
+            }
+            Array.Sort(array, new DescendingComparer());
+            return array;
+        }
+    }
+}
---
+++ AsyncP_lesson2_4/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+/*
+ * Задание 4
+Создайте проект по шаблону "Console Application". Создайте метод с именем «private static double
+FindLastFibonacciNumber(int number)». Метод должен найти и вернуть последнее число из
+последовательности Фибоначчи. Для нахождения последнего числа из последовательности
+Фибоначчи в тело метода вставить следующий код:
+Func<int, double> fib = null;
+fib = (x) => x > 1 ? fib(x - 1) + fib(x - 2) : x;
+return fib.Invoke(number);
+Даже, если вы считаете, что этот код недостаточно оптимизирован, все равно нужно
+использовать его. В этом и смысл, что с помощью такого решения, последовательность числа
+Фибоначчи будет находится намного дольше и с более сильной затратой ресурсов. Поэтому, вам
+нужно вызвать из метода Main этот метод в контексте задачи. Но так как эта операция займет
+много времени, вам нужно использовать флаг TaskCreationOptions.LongRunning, чтобы задача
+выполнялась в контексте потока выполнения Thread и не занимала потоки из пула. Результат
+асинхронной задачи необходимо вывести на экран консоли. Сделайте это с помощью
+продолжения.
+*/
+
+namespace AsyncP_lesson2_4
+{
+    class Program
+    {
+        struct Box
+        {
+            public int number;
+            public Box(int number)
+            {
+                this.number = number;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Task<double> task1 = new Task<double>(FindLastFibonacciNumber, new Box(8));
+            task1.Start();
+
+            task1.ContinueWith((t) => Continuation(t), TaskContinuationOptions.LongRunning);
+            Console.WriteLine("Фибоначи" + task1.Result);
+
+            Console.ReadKey();
+        }
+
+
+        private static double FindLastFibonacciNumber(object obj)
+        {
+            Box box = (Box)obj;
+            return FindLastFibonacciNumber(box.number);
+        }
+        private static double FindLastFibonacciNumber(int number)
+        {
+            Func<int, double> fib = null;
+            fib = (x) => x > 1 ? fib(x - 1) + fib(x - 2) : x;
+            return fib.Invoke(number);
+        }
+        private static void Continuation(Task task)
+        {
+            Console.WriteLine($"Id задачи продолжения - {Task.CurrentId}.");
+            Console.WriteLine($"Продолжение выполнилось в потоке {Thread.CurrentThread.ManagedThreadId}");
+            Console.WriteLine("sdsffffffffrefre");
+        }
+    }
+}
