77499c939d6b38529f87a9d23a1a290eb11293f9
SsamansS <nurlanov.aman@bk.ru>
2021-07-05 22:52:18 +0600

    done EFcore_lesson1_2

---
+++ EFcore_lesson1_2/Form1.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EFcore_lesson1_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            List<User> list = new MyContext().Users.ToList();
+            dataGridView1[0, 0].Value = list[0];
+        }
+
+        private BindingSource bindingSource1 = new BindingSource();
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dataGridView1.DataSource = new MyContext().Users.ToList();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ EFcore_lesson1_2/MyContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFcore_lesson1_2
+{
+    class MyContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public MyContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=AMANURLANUULU;Database=DbForLesson1;Trusted_Connection=True;");
+        }
+    }
+}
