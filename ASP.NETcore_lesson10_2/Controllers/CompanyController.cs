5fe5f96b4316b0f53423fe53030ec07aca722b30
SsamansS <nurlanov.aman@bk.ru>
2021-07-26 17:47:43 +0600

    done ASP.NETcore_lesson10_2

---
+++ ASP.NETcore_lesson10_2/Controllers/CompanyController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication8.Datas;
+using WebApplication8.Models;
+
+namespace WebApplication8.Controllers
+{
+    public class CompanyController:Controller
+    {
+        public IEnumerable<Company> Index()
+        {
+            using (CompanyContext db = new CompanyContext())
+            {
+                db.Companies.Add(new Company() { CompanyName = "Google" });
+                db.Companies.Add(new Company() { CompanyName = "Twitter" });
+                db.Companies.Add(new Company() { CompanyName = "LinkedIn" });
+                db.Companies.Add(new Company() { CompanyName = "Facebook" });
+                db.Companies.Add(new Company() { CompanyName = "SpaceX" });
+                db.SaveChanges();
+            }
+
+            return new CompanyContext().Companies;
+        }
+    }
+}
---
+++ ASP.NETcore_lesson10_2/Datas/CompanyContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication8.Models;
+
+namespace WebApplication8.Datas
+{
+    public class CompanyContext:DbContext
+    {
+        public DbSet<Company> Companies { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb; Database=ASP.NETcore_lesson10_2_Db;Trusted_Connection=True;");
+        }
+    }
+}
